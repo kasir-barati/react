abd8f96c6d756edac5c63d9eb9ad9b1b2691f130
Kasir Barati <kasir.barati@gmail.com>
2024-10-18 08:55:53 +0200

    fix: commit missing file

---
+++ src/components/chat/Messenger.component.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './Messenger.component.css';
+import { contacts } from '../../assets/contacts.asset';
+import { ContactList } from './ContactList.component';
+import { Contact } from '../../types/contact.type';
+import { ChatBox } from './ChatBox.component';
+
+export function Messenger() {
+  const [to, setTo] = useState(contacts[0]);
+
+  function handleSelect(contact: Contact) {
+    setTo(contact);
+  }
+
+  return (
+    <article className="messenger">
+      <h2 className="messenger__header">Messenger app</h2>
+      <form className="messenger__form">
+        <ContactList
+          contacts={contacts}
+          onSelect={handleSelect}
+          selectedContact={to}
+        />
+        <ChatBox key={to.email} contact={to} />
+      </form>
+    </article>
+  );
+}
