28e8d99b6c434213c0d0ff7495aaabc95262a613
Kasir Barati <kasir.barati@gmail.com>
2024-10-22 23:43:18 +0200

    chore: example for useEffectEvent

---
+++ .github/docs/examples/no-passing-useEffectEvent.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect, useEffectEvent } from 'react';
+
+export function Timer() {
+  const [count, setCount] = useState(0);
+  useTimer(() => {
+    setCount(count + 1);
+  }, 1000);
+  return <h1>{count}</h1>;
+}
+
+function useTimer(callback, delay) {
+  const onTick = useEffectEvent(() => {
+    callback();
+  });
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      onTick(); // Good: Only called locally inside an Effect
+    }, delay);
+    return () => {
+      clearInterval(id);
+    };
+  }, [delay]); // No need to specify "onTick" (an Effect Event) as a dependency
+}
---
+++ .github/docs/examples/passing-useEffectEvent.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffectEvent, useEffect } from 'react';
+
+export function Timer() {
+  const [count, setCount] = useState(0);
+
+  const onTick = useEffectEvent(() => {
+    setCount(count + 1);
+  });
+
+  useTimer(onTick, 1000); // Avoid passing useEffectEvent
+
+  return <h1>{count}</h1>;
+}
+
+function useTimer(callback, delay) {
+  useEffect(() => {
+    const id = setInterval(() => {
+      callback();
+    }, delay);
+    return () => {
+      clearInterval(id);
+    };
+  }, [delay, callback]); // Need to specify "callback" in dependencies
+}
