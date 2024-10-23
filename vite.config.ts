432999929de0752fe734e5f0ab9099090b34cb83
Kasir Barati <kasir.barati@gmail.com>
2024-10-23 19:38:14 +0200

    chore: add _config.yml + rename index.html for vite to main.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './index.html',
+      },
+    },
+  },
+  server: {
+    open: '/index.html',
+  },
 });
