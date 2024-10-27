af3aa93a10527842e4dd36698cdab8352ab33589
Kasir Barati <kasir.barati@gmail.com>
2024-10-27 09:50:02 +0100

    chore: add example on how to move useCallback outside of loop

---
+++ .github/docs/examples/useCallback-in-a-loop.jsx
@@ -0,0 +1,60 @@
+export function ReportList({ items }) {
+  return (
+    <article>
+      {items.map((item) => {
+        // Cannot call useCallback in a loop!
+        const handleClick = useCallback(
+          () => sendReport(item),
+          [item],
+        );
+        // Chart component is memoized.
+        return (
+          <figure key={item.id}>
+            <Chart onClick={handleClick} />
+          </figure>
+        );
+      })}
+    </article>
+  );
+}
+
+export function ReportList({ items }) {
+  // useCallback called outside of a loop, at the top level.
+  const handleClick = useCallback((item) => sendReport(item), []);
+
+  return (
+    <article>
+      {items.map((item) => {
+        // Chart component is memoized.
+        return (
+          <figure key={item.id}>
+            <Chart onClick={handleClick} />
+          </figure>
+        );
+      })}
+    </article>
+  );
+}
+
+export function ReportList({ items }) {
+  return (
+    <article>
+      {items.map((item) => (
+        <Report key={item.id} item={item} />
+      ))}
+    </article>
+  );
+}
+
+export function Report({ item }) {
+  // useCallback called at the top level.
+  const handleClick = useCallback(() => {
+    sendReport(item);
+  }, [item]);
+
+  return (
+    <figure>
+      <Chart onClick={handleClick} />
+    </figure>
+  );
+}
